b23c64f676ee3f5a053e6a8a2f913657d996f101
kkkkkxiaofei <brztzxf@gmail.com>
2015-05-16 18:57:46 +0800

    modify the hover css in the photo box

---
+++ script/index.js
@@ -0,0 +1,33 @@
+$().ready(function(){
+
+	var currentIndex = 0;
+    var bigPhotos = $('ul#big-photo li');
+    var photoDescriptions = $('ul#photo-descriotion li');
+
+
+	$('ul#thumb-photo li').hover(function()	{
+		var index = getChildIndex($(this).parent().children(), this);
+		bigPhotoDisplayNone(currentIndex);
+		bigPhotoDisplay(index);
+		currentIndex = index;
+	});
+
+	function getChildIndex (children, ele){
+		for(var i = 0;i < children.length;i ++) {
+			if(children[i] === ele) {
+				return i;
+			}
+		}
+		return current_index;
+	}
+
+	function bigPhotoDisplayNone(index) {
+		bigPhotos.eq(index).css("display","none");
+	}
+
+	function bigPhotoDisplay(index) {
+		bigPhotos.eq(index).css("display","list-item");
+	}
+
+
+})
