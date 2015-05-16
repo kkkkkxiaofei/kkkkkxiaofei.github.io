b079e1655e5054f7a0cab36e99ba220428816510
kkkkkxiaofei <brztzxf@gmail.com>
2015-05-16 23:31:33 +0800

    photo can automatically slide

--- _site/script/index.js
+++ _site/script/index.js
@@ -1,16 +1,18 @@
 $().ready(function(){
-
-	var currentIndex = 0;
+	var MAX = 9;
+	var MIN = 0;
+	var LEN = MAX - MIN + 1;
+	var currentIndex = 1;
     var bigPhotos = $('ul#big-photo li');
     var photoDescriptions = $('ul#photo-descriotion li');
+    var thumbPhotos = $('span.mask');
 
-
-	// $('ul#thumb-photo li').hover(function()	{
-	// 	var index = getChildIndex($(this).parent().children(), this);
-	// 	bigPhotoDisplayNone(currentIndex);
-	// 	bigPhotoDisplay(index);
-	// 	currentIndex = index;
-	// });
+	$('ul#thumb-photo li').hover(function()	{
+		// var index = getChildIndex($(this).parent().children(), this);
+		// bigPhotoDisplayNone(currentIndex);
+		// bigPhotoDisplay(index);
+		// currentIndex = index;
+	});
 
 	function getChildIndex (children, ele){
 		for(var i = 0;i < children.length;i ++) {
@@ -18,16 +20,55 @@ $().ready(function(){
 				return i;
 			}
 		}
-		return current_index;
+		return currentIndex;
 	}
+
 
 	function bigPhotoDisplayNone(index) {
 		bigPhotos.eq(index).css("display","none");
 	}
 
 	function bigPhotoDisplay(index) {
-		bigPhotos.eq(index).css("display","list-item");
+		bigPhotos.eq(index).css("display","block");
 	}
 
+	function activeThumbPhoto(index) {
+		thumbPhotos.eq(index).removeClass('mask');
+	}
+
+	function unActiveThumbPhoto(index) {
+		thumbPhotos.eq(index).addClass('mask');
+	}
+
+	setInterval(function() {
+		
+	}, 1000);
+
+	$('.focus_switch').click(test);
+
+	(function initThumbAndBigPhoto(){
+		activeThumbPhoto(MIN);
+		bigPhotoDisplay(MIN);
+	})();
+
+	setInterval(test, 1500);
+
+	function test(){
+		if(currentIndex > MIN){
+			unActiveThumbPhoto(currentIndex -1);
+			bigPhotoDisplayNone(currentIndex -1);
+		}else {
+			unActiveThumbPhoto(MAX);
+			bigPhotoDisplayNone(MAX);
+		}
+
+		activeThumbPhoto(currentIndex);
+		bigPhotoDisplay(currentIndex);
+		if(currentIndex < MAX){
+			currentIndex++;
+		}else{
+			currentIndex = MIN;
+		}
+	}
 
 })
