0dc5bb97f113ada63a551cffc6d30de8f71507f9
kkkkkxiaofei <brztzxf@gmail.com>
2015-05-16 19:25:43 +0800

    modify the hover style

--- script/index.js
+++ script/index.js
@@ -5,12 +5,12 @@ $().ready(function(){
     var photoDescriptions = $('ul#photo-descriotion li');
 
 
-	$('ul#thumb-photo li').hover(function()	{
-		var index = getChildIndex($(this).parent().children(), this);
-		bigPhotoDisplayNone(currentIndex);
-		bigPhotoDisplay(index);
-		currentIndex = index;
-	});
+	// $('ul#thumb-photo li').hover(function()	{
+	// 	var index = getChildIndex($(this).parent().children(), this);
+	// 	bigPhotoDisplayNone(currentIndex);
+	// 	bigPhotoDisplay(index);
+	// 	currentIndex = index;
+	// });
 
 	function getChildIndex (children, ele){
 		for(var i = 0;i < children.length;i ++) {
