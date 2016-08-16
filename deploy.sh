c20526a8daefef692b150357c0acfe04bde804d4
Xiaofei <xifzhang@thoughtworks.com>
2016-08-16 20:56:00 +0800

    deploy@0816-v1

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
-cd ../kkkkkxiaofei.github.io
 cp -r ../blog-dev/_site/* .
+echo "Please enter the version:"
+read version
+date_time=`date +'%m%d'`
+git add .
+git ci -am "deploy@${date_time}-${version}"
+git push origin gh-pages
